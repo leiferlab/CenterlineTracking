3575a1a65ec71aa1c0168072625b57c2d4981a76
ashleylinder <linder@princeton.edu>
2015-06-15 11:35:09 -0400

    twaeked parameters for when worm is clsoe to self

--- SnakeySnakey.m
+++ SnakeySnakey.m
@@ -8,10 +8,10 @@ ConCrit = .08; %Convergence criteria
 kappa = 800;     % Weight of the image force as a whole
 sigma = 10;   %Smoothing for the derivative calculations in the image
 alpha = 0; % Bending modulus
-beta = 400;
+beta = 200;
 nu = 40;  %tip force
 mu1 =100; %repel force
-cd = 16; %cutoff distance for repel force
+cd = 30; %cutoff distance for repel force
 
 
 
